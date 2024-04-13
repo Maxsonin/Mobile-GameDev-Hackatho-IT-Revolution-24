24dbf6c9f95b6b48aca8d323a6459d3ceeb7901b
Anteon1939 <164662944+Anteon1939@users.noreply.github.com>
2024-04-13 15:49:37 +0300

    mainmenu

---
+++ scenes/mainmenu/Control.gd
@@ -0,0 +1,10 @@
+extends Control
+ 
+
+
+
+
+
+func _on_touch_screen_button_pressed():
+	print("pres")
+	pass # Replace with function body.
---
+++ scenes/mainmenu/main_menu.gd
@@ -0,0 +1,6 @@
+extends Control
+
+
+
+func _on_touch_screen_button_pressed():
+	print("play")
