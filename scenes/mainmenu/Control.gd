7c652fee7e8d22e9ac76de95d0e627ef2c3962a3
Anteon1939 <164662944+Anteon1939@users.noreply.github.com>
2024-04-13 20:03:29 +0300

    tile

--- scenes/mainmenu/Control.gd
+++ scenes/mainmenu/Control.gd
@@ -6,5 +6,5 @@ extends Control
 
 
 func _on_touch_screen_button_pressed():
-	print("pres")
+	get_tree().change_scene_to_file("res://scenes/worlds/MaceForgingWorld.tscn")
 	pass # Replace with function body.
