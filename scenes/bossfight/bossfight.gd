9038b54b4379a08b4b4b0382c44944aca4ce8dc2
Zelefin <nagangoroh@gmail.com>
2024-04-14 09:44:15 +0300

    added transitions for boss fight

--- scenes/bossfight/bossfight.gd
+++ scenes/bossfight/bossfight.gd
@@ -1,5 +1,9 @@
 extends Node2D
+@onready var scene_trasition: CanvasLayer = $SceneTrasition
+@onready var main_character: CharacterBody2D = $MainCharacter
 
+var played = false
+var victory = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -7,4 +11,26 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if main_character.position.x > 300 and victory:
+		change_scene()
+
+func change_scene():
+	if !played:
+		played = true
+		await get_tree().create_timer(1).timeout
+		scene_trasition.transition()
+
+
+func _on_generator_player_got_hit() -> void:
+	change_scene()
+
+
+func _on_scene_trasition_transitioned() -> void:
+	if victory:
+		get_tree().change_scene_to_file("res://scenes/mainmenu/main_menu.tscn")
+	else:
+		get_tree().change_scene_to_file("res://scenes/deathscreen/death_screen.tscn")
+
+
+func _on_real_boss_dragon_killed() -> void:
+	victory = true
