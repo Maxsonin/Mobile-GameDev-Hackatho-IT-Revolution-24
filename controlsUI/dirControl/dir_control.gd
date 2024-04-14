2a6b83052cbd91432c16feec7ffb153d8d841bf9
Anteon1939 <164662944+Anteon1939@users.noreply.github.com>
2024-04-14 10:29:26 +0300

    joystik

--- controlsUI/dirControl/dir_control.gd
+++ controlsUI/dirControl/dir_control.gd
@@ -1,16 +1,64 @@
 extends Control
-
 @onready var top_button = %TopButton
 @onready var down_button = %DownButton
 @onready var left_button = %LeftButton
 @onready var right_button = %RightButton
 
-func _process(delta: float):
-	if top_button.pressed:
-		Input.action_press("Up")
-	elif down_button.pressed:
-		Input.action_press("Down")
-	elif left_button.pressed:
-		Input.action_press("Left")
-	elif right_button.pressed:
-		Input.action_press("Right")
+func _on_top_button_pressed():
+	Input.action_release("Down")
+	Input.action_release("Left")
+	Input.action_release("Right")
+	Input.action_press("Up")
+	
+
+
+func _on_down_button_pressed():
+	Input.action_release("Left")
+	Input.action_release("Right")
+	Input.action_release("Up")
+	Input.action_press("Down")
+
+
+func _on_left_button_pressed():
+	Input.action_release("Down")
+	Input.action_release("Left")
+	Input.action_release("Right")
+	Input.action_release("Up")
+	Input.action_press("Left")
+
+
+func _on_right_button_pressed():
+	Input.action_release("Down")
+	Input.action_release("Left")
+	Input.action_release("Right")
+	Input.action_release("Up")
+	Input.action_press("Right")
+
+
+
+
+func _on_top_button_released():
+	Input.action_release("Down")
+	Input.action_release("Left")
+	Input.action_release("Right")
+	Input.action_release("Up")
+
+func _on_down_button_released():
+	Input.action_release("Down")
+	Input.action_release("Left")
+	Input.action_release("Right")
+	Input.action_release("Up")
+
+
+func _on_left_button_released():
+	Input.action_release("Down")
+	Input.action_release("Left")
+	Input.action_release("Right")
+	Input.action_release("Up")
+
+
+func _on_right_button_released():
+	Input.action_release("Down")
+	Input.action_release("Left")
+	Input.action_release("Right")
+	Input.action_release("Up")
